ec998cf12e8d824bc2466a560f14b4955815e584
Semyon Zasov <semenmaslina@gmail.com>
2022-08-19 16:02:56 +0300

    add color-secondary + nav

---
+++ apps/web/components/Nav/Nav.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import styles from './nav.module.scss';
+
+export const Nav: FC = () => {
+  return <div className={styles.nav}></div>;
+};
--- apps/web/hoc/layout/Layout.tsx
+++ apps/web/hoc/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from 'react';
+import { Nav } from '../../components/Nav/Nav';
 import styles from './layout.module.scss';
 
 interface ILayout {
@@ -8,7 +9,9 @@ interface ILayout {
 export const Layout: FC<ILayout> = ({ children }) => {
   return (
     <div className={styles.layout}>
-      <nav className={styles.nav}>nav</nav>
+      <nav className={styles.nav}>
+        <Nav />
+      </nav>
       <main className={styles.main}>{children}</main>
       <footer className={styles.footer}>footer</footer>
     </div>
