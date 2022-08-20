04eb196c6f67e46068e5f16a249e915b71f2a172
Semyon Zasov <semenmaslina@gmail.com>
2022-08-20 15:12:51 +0300

    add logo

--- apps/web/components/Nav/Nav.tsx
+++ apps/web/components/Nav/Nav.tsx
@@ -1,6 +1,11 @@
 import { FC } from 'react';
+import { Logo } from '../logo/Logo';
 import styles from './nav.module.scss';
 
 export const Nav: FC = () => {
-  return <div className={styles.nav}></div>;
+  return (
+    <div className={styles.nav}>
+      <Logo />
+    </div>
+  );
 };
