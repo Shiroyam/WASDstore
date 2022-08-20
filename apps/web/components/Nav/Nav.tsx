dfeeb419f177182c84ddd0d806416625a52da9b8
Semyon Zasov <semenmaslina@gmail.com>
2022-08-20 21:57:11 +0300

    add nav content

--- apps/web/components/Nav/Nav.tsx
+++ apps/web/components/Nav/Nav.tsx
@@ -1,11 +1,29 @@
 import { FC } from 'react';
 import { Logo } from '../logo/Logo';
+import { navItems } from '../../helpers/data/navItems';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+import LoginIcon from '@mui/icons-material/Login';
 import styles from './nav.module.scss';
 
 export const Nav: FC = () => {
   return (
     <div className={styles.nav}>
-      <Logo />
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <div className={styles.items}>
+        {navItems.map((navItem) => (
+          <div className={styles.item} key={navItem.id}>
+            {navItem.item}
+          </div>
+        ))}
+      </div>
+      <div className={styles.icons}>
+        <LoginIcon className={styles.icon} />
+        <ShoppingCartIcon className={styles.icon} />
+        <SearchIcon className={styles.icon} />
+      </div>
     </div>
   );
 };
